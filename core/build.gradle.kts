332ea35d70661e36c4fa235766a6f63fcbca21ee
Bin Zhang <ebnbin@gmail.com>
2024-11-28 04:45:24 -0800

    Convert build.gradle to build.gradle.kts for module 'core'

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,9 +1,9 @@
-import Dependencies
-
-apply plugin: 'kotlin'
+plugins {
+    kotlin("jvm")
+}
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(BuildConfig.JAVA_VERSION))
 
 dependencies {
-  api project(':kgdx')
+    api(project(":kgdx"))
 }
