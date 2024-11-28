e0a26cff657c20c75a835ca8d8e0296840f4cb62
Bin Zhang <ebnbin@gmail.com>
2024-11-28 00:34:16 -0800

    Rename DesktopLauncher

--- desktop/src/main/kotlin/dev/ebnbin/warmoji/desktop/StartupHelper.kt
+++ desktop/src/main/kotlin/dev/ebnbin/warmoji/desktop/StartupHelper.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 //Note, the above license and copyright applies to this file only.
-package dev.ebnbin.warmoji.lwjgl3
+package dev.ebnbin.warmoji.desktop
 
 import org.lwjgl.system.macosx.LibC
 import java.io.BufferedReader
