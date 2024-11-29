ce4d27c35a1848da7c781124dc98b1e32efc81ba
Bin Zhang <ebnbin@gmail.com>
2024-11-28 23:04:08 -0800

    Draw all emojis

--- core/src/main/kotlin/dev/ebnbin/warmoji/WarMoji.kt
+++ core/src/main/kotlin/dev/ebnbin/warmoji/WarMoji.kt
@@ -1,6 +1,8 @@
 package dev.ebnbin.warmoji
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.ScreenUtils
@@ -9,59 +11,101 @@ import com.mazatech.gdx.SVGAssetsGDX
 import dev.ebnbin.kgdx.Game
 import dev.ebnbin.kgdx.game
 import java.util.zip.ZipInputStream
+import kotlin.math.ceil
 import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 val warMoji: WarMoji
     get() = game as WarMoji
 
 class WarMoji : Game() {
+    private class DrawingData(
+        val size: Float,
+        val tileList: List<Tile>,
+    ) {
+        class Tile(
+            val texture: Texture,
+            val x: Float,
+            val y: Float,
+        )
+    }
+
+    private lateinit var orthographicCamera: OrthographicCamera
     private lateinit var spriteBatch: SpriteBatch
-    private lateinit var texture: Texture
+    private lateinit var emojiTextureList: List<Texture>
+    private lateinit var drawingData: DrawingData
 
     override fun create() {
         super.create()
+        orthographicCamera = OrthographicCamera()
         spriteBatch = SpriteBatch()
-        texture = createEmojiTexture("1F600")
+        emojiTextureList = createEmojiTextureList()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
+        orthographicCamera.setToOrtho(false, width.toFloat(), height.toFloat())
+        drawingData = createDrawingData(emojiTextureList, width.toFloat(), height.toFloat())
     }
 
     override fun render() {
         super.render()
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f)
+        ScreenUtils.clear(Color.WHITE)
+        spriteBatch.projectionMatrix = orthographicCamera.combined
         spriteBatch.begin()
-        spriteBatch.draw(texture, 140f, 210f)
+        drawingData.tileList.forEach { tile ->
+            spriteBatch.draw(tile.texture, tile.x, tile.y, drawingData.size, drawingData.size, 0, 0,
+                tile.texture.width, tile.texture.height, false, true)
+        }
         spriteBatch.end()
     }
 
     override fun dispose() {
-        texture.dispose()
+        emojiTextureList.forEach(Texture::dispose)
         spriteBatch.dispose()
         super.dispose()
     }
 
     companion object {
-        private fun createEmojiTexture(hexcode: String): Texture {
+        private fun createEmojiTextureList(): List<Texture> {
+            val emojiTextureList = mutableListOf<Texture>()
+            val svgAssetsConfigGDX = SVGAssetsConfigGDX(Gdx.graphics.backBufferWidth, Gdx.graphics.backBufferHeight,
+                Gdx.graphics.ppiX)
+            val svgAssetsGDX = SVGAssetsGDX(svgAssetsConfigGDX)
             val zipFileHandle = Gdx.files.internal("openmoji-svg-color.zip")
             ZipInputStream(zipFileHandle.read(DEFAULT_BUFFER_SIZE)).use { zipInputStream ->
                 while (true) {
-                    val zipEntry = zipInputStream.nextEntry ?: break
-                    if (zipEntry.name != "$hexcode.svg") {
-                        zipInputStream.closeEntry()
-                        continue
-                    }
+                    zipInputStream.nextEntry ?: break
                     val svgText = zipInputStream.bufferedReader().readText()
                     zipInputStream.closeEntry()
-                    val svgAssetsConfigGDX = SVGAssetsConfigGDX(Gdx.graphics.backBufferWidth,
-                        Gdx.graphics.backBufferHeight, Gdx.graphics.ppiX)
-                    val svgAssetsGDX = SVGAssetsGDX(svgAssetsConfigGDX)
                     val svgDocument = svgAssetsGDX.createDocument(svgText)
                     val texture = svgAssetsGDX.createTexture(svgDocument, svgDocument.viewport.width.roundToInt(),
-                        svgDocument.viewport.width.roundToInt())
+                        svgDocument.viewport.height.roundToInt())
                     svgDocument.dispose()
-                    svgAssetsGDX.dispose()
-                    return texture
+                    emojiTextureList.add(texture)
                 }
             }
-            error(Unit)
+            svgAssetsGDX.dispose()
+            return emojiTextureList
+        }
+
+        private fun createDrawingData(emojiTextureList: List<Texture>, width: Float, height: Float): DrawingData {
+            if (width <= 0f || height <= 0f) {
+                return DrawingData(0f, emptyList())
+            }
+            var columns = ceil(sqrt(emojiTextureList.size / height * width)).toInt()
+            var rows = ceil(emojiTextureList.size.toFloat() / columns).toInt()
+            while (width / columns * rows > height) {
+                ++columns
+                rows = ceil(emojiTextureList.size.toFloat() / columns).toInt()
+            }
+            val size = width / columns
+            val tileList = emojiTextureList.mapIndexed { index, emojiTexture ->
+                val x = index % columns * size
+                val y = index / columns * size
+                DrawingData.Tile(emojiTexture, x, y)
+            }
+            return DrawingData(size, tileList)
         }
     }
 }
